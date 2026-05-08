ed9f918c0b2b673bb66c830b5fd2324fbd6788c3
FeFe488 <helge_schulz@outlook.de>
2026-05-08 15:38:46 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,6 @@ const Note = require('./models/note')
 
 const app = express()
 app.use(express.json())
-app.use(express.static('dist'))
-
 
 let notes = []
 
@@ -18,19 +16,13 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(express.static('dist'))
 app.use(requestLogger)
 
 
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>')
-// })
-
 app.get('/', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes)
-  })
+  response.send('<h1>Hello World!</h1>')
 })
-
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
